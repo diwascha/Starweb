b8b25ce45fe8a2bbf9548db2687f9caec146544d
Diwas Chaulagain <diwasphoto@gmail.com>
2025-09-09 09:01:11 +0000

    shrawan data is missing

--- src/app/hr/bonus/page.tsx
+++ src/app/hr/bonus/page.tsx
@@ -21,7 +21,7 @@ const nepaliMonths = [
     { value: 0, name: "Baishakh" }, { value: 1, name: "Jestha" }, { value: 2, name: "Ashadh" },
     { value: 3, name: "Shrawan" }, { value: 4, name: "Bhadra" }, { value: 5, name: "Ashwin" },
     { value: 6, name: "Kartik" }, { value: 7, name: "Mangsir" }, { value: 8, name: "Poush" },
-    { value: 9, name: "Magh" }, { value: 10, name: "Falgun" }, { value: 11, "name": "Chaitra" }
+    { value: 9, name: "Magh" }, { value: 10, name: "Falgun" }, { value: 11, name: "Chaitra" }
 ];
 
 const customEmployeeOrder = [
