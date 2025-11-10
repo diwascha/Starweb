e72ee17013be3a8f405fc81eb5b93696d1d21aa5
Diwas Chaulagain <diwasphoto@gmail.com>
2025-11-10 06:08:42 +0000

    Same issue repeated. find the creative solution. > Server Actions are no

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  serverActions: false,
   typescript: {
     ignoreBuildErrors: true,
   },
