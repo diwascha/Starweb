59e0981cbcbba1b27c1d767e6fe82aca95f84ddf
Diwas Chaulagain <diwasphoto@gmail.com>
2025-11-10 04:25:36 +0000

    C:\Users\User\Documents\GitHub\Starweb>npm run electron:build
    > nextn@0

--- src/app/fleet/transactions/purchase/view/page.tsx
+++ src/app/fleet/transactions/purchase/view/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 import { Suspense, useEffect, useState } from 'react';
-import PurchaseViewClient from './_components/PurchaseViewClient';
+import PurchaseViewClient from './PurchaseViewClient';
 import { getTransaction, onTransactionsUpdate } from '@/services/transaction-service';
 import { onVehiclesUpdate } from '@/services/vehicle-service';
 import { onPartiesUpdate } from '@/services/party-service';
